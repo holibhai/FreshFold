34ab9d97a462998c66454c1c394b2cfeee07c913
holibhai <146805057+holibhai@users.noreply.github.com>
2025-05-06 21:31:47 +0530

    initial commit

---
+++ src/main/java/com/crudoperation/freshfold/FreshFoldApplication.java
@@ -0,0 +1,13 @@
+package com.crudoperation.freshfold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshFoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshFoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crudoperation/freshfold/FreshFoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudoperation.freshfold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshFoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
